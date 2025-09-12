596dc52ca38f61cf00f6fa44c6c2b43f25ab6893
liwenchiou <aa771117@gmail.com>
2025-09-12 11:32:00 +0800

    第一次修改完成

--- assets/js/components/swiper.js
+++ assets/js/components/swiper.js
@@ -35,3 +35,46 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+
+const top_swiper = new Swiper(".top-swiper", {
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true, // true 表示交叉淡入淡出，過渡更平滑
+  },
+  //間距24px
+  spaceBetween: 24,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // 左右箭頭
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    // nextEl: ".swiper-button-next",
+    // prevEl: ".custom-prev",
+  },
+  //呈現三個
+  slidesPerView: 1,
+  // 滾動條
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  autoplay: true,
+
+  // 響應式設定（類似 Bootstrap 的斷點）
+  breakpoints: {
+    // 0px 以上（手機）
+    0: {
+      slidesPerView: 1,
+    },
+    // 768px 以上（平板）
+    768: {
+      slidesPerView: 1,
+    },
+    // 992px 以上（桌機）
+    992: {
+      slidesPerView: 1,
+    },
+  },
+});
+
